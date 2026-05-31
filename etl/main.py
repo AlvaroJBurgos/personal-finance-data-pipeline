aed15a94f2b2ca0fd0df5a916e6f80a690a862f3
AlvaroJBurgos <alvaroburgosgarita@gmail.com>
2026-05-31 21:05:37 +0200

    Rename folder to ETL and added initial logging

---
+++ etl/main.py
@@ -0,0 +1,42 @@
+from ingestion import load_raw_data
+from transformation import transform_data
+from modeling import create_star_schema
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def main():
+    
+    logging.info('Starting ETL pipeline')
+
+    # Extract
+    try:
+        raw_data = load_raw_data()
+        logging.info('Loading raw files')
+    except Exception as e:
+        logging.error(f'Extaction failed: {e}')
+        raise
+    
+    
+    # Transform
+    try:
+        transformed_data = transform_data(raw_data)
+        logging.info('Transformation completed')
+    except Exception as e:
+        logging.error(f'Transformation failed: {e}')
+        raise
+    
+    # Model + Export
+    try:
+        fact_transactions, dim_category, dim_date = create_star_schema(transformed_data)
+        logging.info("Successfuly created the star schema")
+    except Exception as e:
+        logging.error(f'Modeling/Export failed: {e}')
+        raise
+
+if __name__ == '__main__':
+    main()
