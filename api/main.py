cb5ccc2f193a6b128d2e9cc4475bd27001c79f47
AlvaroJBurgos <alvaroburgosgarita@gmail.com>
2026-07-12 20:57:42 +0200

    Created a Bakend API with FastAPI and added the routers

--- api/main.py
+++ api/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, APIRouter
+from api.routers.transactions import router as transactions_router
+from api.routers.categories import router as categories_router
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+app.include_router(transactions_router, prefix="/transactions")
+app.include_router(categories_router, prefix="/categories")
