456a4bda042187e1d331c2eea35c20f791feaab8
AlvaroJBurgos <alvaroburgosgarita@gmail.com>
2026-07-12 20:57:04 +0200

    Added routers for the API

---
+++ api/routers/categories.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, APIRouter
+from api.services.data_service import get_categories
+
+router = APIRouter()
+
+@router.get("/")
+def categories():
+    df_cagetories = get_categories()
+    dict_categories = df_cagetories.to_dict("records")
+    return dict_categories
+    
+    
+    
---
+++ api/routers/transactions.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, APIRouter
+from api.services.data_service import get_transactions
+
+router = APIRouter()
+
+@router.get("/")
+def transactions(year: int | None = None, month: str | None = None):
+    df_transactions = get_transactions(year, month)
+    dict_transactions = df_transactions.to_dict("records")
+    return dict_transactions
+
