87d01d5c1c1c0ef5bb6ea366b5174966aa583acf
AlvaroJBurgos <alvaroburgosgarita@gmail.com>
2026-07-13 20:31:19 +0200

    Added the first monthly summary API, returning total amount

--- api/routers/transactions.py
+++ api/routers/transactions.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter
 from api.services.data_service import get_transactions
 
 router = APIRouter()
@@ -8,4 +8,3 @@ def transactions(year: int | None = None, month: str | None = None):
     df_transactions = get_transactions(year, month)
     dict_transactions = df_transactions.to_dict("records")
     return dict_transactions
-
