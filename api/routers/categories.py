4f81571d4fd4eec36e6b9c46d38a612ef2f18088
AlvaroJBurgos <alvaroburgosgarita@gmail.com>
2026-07-21 21:30:18 +0200

    Added Pydantic BaseModels to the API responses and refactor the data service file

--- api/routers/categories.py
+++ api/routers/categories.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from api.services.data_service import get_categories
+from api.models import Category
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/", response_model=list[Category])
 def categories():
     df_cagetories = get_categories()
     dict_categories = df_cagetories.to_dict("records")
